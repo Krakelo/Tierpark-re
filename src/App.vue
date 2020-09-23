fb791186500c6267a99097abc8acd05b2a6bf812
Krakelo <vasilij.slabinski@yahoo.com>
2020-09-24 01:01:41 +0200

    Fix

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
   <div id="app">
     <backimg />
+    <infobuttonY/>
   </div>
 </template>
 
 <script>
 import backimg from '@/components/Image.vue'
+import Infobutton from "./components/InfoButton";
 
 export default {
   name: 'App',
   components: {
+    Infobutton,
     backimg
   }
 }
