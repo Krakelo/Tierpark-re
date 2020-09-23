d5481502be17a21afaad43ba462491868265c291
jpkmiller <j.mueller5021@gmail.com>
2020-09-24 01:01:23 +0200

    added props to infobutton

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,24 @@
 <template>
   <div id="app">
     <backimg />
+    <infobutton :x="x" :y="456" />
   </div>
 </template>
 
 <script>
 import backimg from '@/components/Image.vue'
+import infobutton from '@/components/InfoButton.vue'
 
 export default {
   name: 'App',
   components: {
-    backimg
+    backimg,
+    infobutton
+  },
+  data () {
+    return {
+      x: 123
+    }
   }
 }
 </script>
