9623b29930ff8d59a9c97c06c4ee251d88fe550b
Krakelo <vasilij.slabinski@yahoo.com>
2020-09-24 02:38:39 +0200

    Carousel + Clicked

--- src/components/button/InfoButton.vue
+++ src/components/button/InfoButton.vue
@@ -1,14 +1,18 @@
 <template>
   <div>
-    <b-button :style="{left: x + 'vw', top: y + 'vw'}">
+    <b-button @click="click" :style="{left: x + 'vw', top: y + 'vw'}">
       <b-icon-info font-scale="1.4" />
     </b-button>
+    <contentvue v-if="clicked"/>
+
   </div>
 </template>
 
 <script>
+import contentvue from '../Content'
 export default {
   name: 'infobutton',
+  components: { contentvue },
   props: {
     id: {
       type: String,
@@ -23,9 +27,15 @@ export default {
       required: true
     }
   },
+  data () {
+    return {
+      clicked: false
+    }
+  },
   methods: {
     click: function () {
-      alert('hi')
+      this.clicked = !this.clicked
+      console.log(this.clicked)
     }
   }
 }
