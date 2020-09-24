237078e1f315f601ffe5cf7d1e48189a88e40784
jpkmiller <j.mueller5021@gmail.com>
2020-09-24 05:50:22 +0200

    fixed carousel and updated json

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,8 @@
 </template>
 
 <script>
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 import backimg from '@/components/Image.vue'
 
 export default {
--- src/components/Content.vue
+++ src/components/Content.vue
@@ -1,54 +1,24 @@
 <template>
-<div>
-  <b-carousel class="overlay-content"
-    :style="{left: x / 2 + 'vw', top: y + 2 + 'vw'}"
-    v-model="slide"
-    :interval="10000"
-    controls
-    indicators
-    background="#ababab"
-    img-width="1024"
-    img-height="480"
-    style="text-shadow: 1px 1px 2px #333;"
-    @sliding-start="onSlideStart"
-    @sliding-end="onSlideEnd"
-  >
-    <!-- Text slides with image -->
-    <b-carousel-slide
-      caption="First slide"
-      text="Nulla vitae elit libero, a pharetra augue mollis interdum."
-      img-src="https://picsum.photos/1024/480/?image=52"
-    ></b-carousel-slide>
-
-      <!-- Slides with custom text -->
-      <b-carousel-slide img-src="https://picsum.photos/1024/480/?image=54">
-        <h1>Hello world!</h1>
-      </b-carousel-slide>
-
-      <!-- Slides with image only -->
-      <b-carousel-slide img-src="https://picsum.photos/1024/480/?image=58"></b-carousel-slide>
-
-      <!-- Slides with img slot -->
-      <!-- Note the classes .d-block and .img-fluid to prevent browser default image alignment -->
-      <b-carousel-slide>
-        <template v-slot:img>
-          <img
-            class="d-block img-fluid w-100"
-            width="1024"
-            height="480"
-            src="https://picsum.photos/1024/480/?image=55"
-            alt="image slot"
-          >
-        </template>
-      </b-carousel-slide>
+  <div class="overlay-content" :style="{left: x + 'vw', top: y + 18 + 'vw'}">
+    <b-carousel
+      :interval="5000"
+      indicators
+      fade
+      background="#ababab"
+      img-width="1024"
+      img-height="480"
+      style="text-shadow: 1px 1px 2px #333;"
+    >
+      <template v-for="(b, idx) in button.content">
+        <b-carousel-slide
+          :key="idx"
+        >
+          <h1>{{ b.text }}</h1>
+          <img :src="require('@/assets/hintergrund/' + b.img)" style="width: auto;" />
+        </b-carousel-slide>
+      </template>
     </b-carousel>
-
-    <p class="mt-4">
-      Slide #: {{ slide }}<br>
-      Sliding: {{ sliding }}
-    </p>
-
-</div>
+  </div>
 </template>
 
 <script>
@@ -67,14 +37,6 @@ export default {
       required: true
     }
   },
-  methods: {
-    onSlideStart (slide) {
-      this.sliding = true
-    },
-    onSlideEnd (slide) {
-      this.sliding = false
-    }
-  },
   computed: {
     x: function () {
       return this.button.coordinates.x || 0
@@ -89,5 +51,9 @@ export default {
 <style>
   .overlay-content {
     position: absolute;
+  }
+
+  .carousel-inner {
+    overflow:visible;
   }
 </style>
